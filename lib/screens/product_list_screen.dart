9c25139d8acea69c18f3f77cffa7755135857830
Alef <alefsilvaf@outlook.com>
2023-10-20 22:30:23 -0300

    Atualizando lista de produtos

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -83,13 +83,16 @@ class _ProductListScreenState extends State<ProductListScreen> {
               },
             ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          await Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => ProductRegistrationScreen(),
             ),
           );
+
+          // Atualize a lista de produtos quando voltar da tela de registro
+          _refreshProductList();
         },
         child: Icon(Icons.add),
       ),
