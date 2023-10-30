2a36055764e113c2467e181b2b11187d5ad3fafc
Alef <alefsilvaf@outlook.com>
2023-10-30 20:27:53 -0300

    Implementação da Tela de venda

--- lib/screens/supplier_list_screen.dart
+++ lib/screens/supplier_list_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../database/supplier_database.dart';
 import 'supplier_registration_screen.dart';
 
@@ -33,47 +36,117 @@ class _SupplierListScreenState extends State<SupplierListScreen> {
       appBar: AppBar(
         title: Text('Fornecedores'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: suppliers.length,
-              itemBuilder: (context, index) {
-                final supplier = suppliers[index];
-                return Card(
-                  child: ListTile(
-                    title: Text(supplier['name']),
-                    subtitle: Text(supplier['email'] ?? ''),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            suppliers.isEmpty
+                ? Center(
+                    child: Text('Ainda não há fornecedores cadastrados.'),
+                  )
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: suppliers.length,
+                      itemBuilder: (context, index) {
+                        final supplier = suppliers[index];
+                        return Card(
+                          margin: EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: Text(
+                              supplier['name'].toString().toUpperCase(),
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 8.0),
+                                Text('Telefone: ${supplier['phone_number']}'),
+                                SizedBox(height: 8.0),
+                                Text('E-mail: ${supplier['email']}'),
+                                SizedBox(height: 8.0),
+                                Text('Endereço: ${supplier['address']}'),
+                                SizedBox(height: 8.0),
+                              ],
+                            ),
+                            onLongPress: () {
+                              // Copie os detalhes do fornecedor para a área de transferência
+                              final supplierDetails =
+                                  'Nome: ${supplier['name']}\nTelefone: ${supplier['phone_number']}\nE-mail: ${supplier['email']}\nEndereço: ${supplier['address']}';
+
+                              Clipboard.setData(
+                                  ClipboardData(text: supplierDetails));
+
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(SnackBar(
+                                content: Text('Detalhes copiados'),
+                              ));
+                            },
+                            trailing: GestureDetector(
+                              onTap: () async {
+                                _launchWhatsapp('${supplier['phone_number']}');
+                              },
+                              child: Container(
+                                alignment: Alignment.bottomLeft,
+                                height: 60.0,
+                                width: 48.0,
+                                child: FaIcon(
+                                  FontAwesomeIcons.whatsapp,
+                                  color: Colors.green,
+                                  size: 35.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                );
-              },
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) =>
-                      SupplierRegistrationScreen(), // Substitua pelo nome da tela de cadastro de fornecedores
-                ));
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                margin: EdgeInsets.all(35.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => SupplierRegistrationScreen(),
+                    ));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Cadastrar Novo Fornecedor',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
-              child: Text(
-                'Cadastrar Fornecedor',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  void _launchWhatsapp(String text) async {
+    // Remova todos os caracteres não numéricos do texto
+    final phoneNumber = text.replaceAll(RegExp(r'[^0-9]'), '');
+    // Verifique se o número não está vazio
+    if (phoneNumber.isNotEmpty) {
+      final whatsappUrl = Uri.parse('https://wa.me/55$phoneNumber');
+      await launchUrl(whatsappUrl, mode: LaunchMode.externalApplication);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Número de telefone inválido.'),
+      ));
+    }
   }
 }
